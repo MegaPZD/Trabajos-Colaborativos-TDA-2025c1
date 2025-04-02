fa10d3069d51f80a652dc7b3fb02212082167134
Moyano Andres <ammoyano@fi.uba.ar>
2025-04-02 18:33:04 -0300

    generador de carreras y ajustado el codigo greedy

--- Problema1TP1.py
+++ Problema1TP1.py
@@ -1,21 +1,17 @@
 def postas_hitratacion(mapa_carrera):
 
     mapa_carrera.sort(key=lambda ciudad: ciudad[1])
-
     postas_hitratacion = []
-    max_distancia = 0
-    
-    for i in range(1, len(mapa_carrera)):
-        distancia_actual = mapa_carrera[i][1] - mapa_carrera[i-1][1]
-        if distancia_actual + max_distancia <= 7:
-            max_distancia += distancia_actual
-        elif distancia_actual > 7 and i != len(mapa_carrera) - 1:
-            max_distancia = 0
-        else:
-            postas_hitratacion.append(mapa_carrera[i-1])
-            max_distancia = 0  
+    ultima_posta = 0
+    cantidad_postas = len(mapa_carrera)
 
-    if max_distancia > 0:
+    for i in range(1, cantidad_postas):
+        distancia_actual = mapa_carrera[i][1] - mapa_carrera[ultima_posta][1]
+        
+        if distancia_actual > 7:
+            postas_hitratacion.append(mapa_carrera[i - 1])
+            ultima_posta = i - 1
+    if ultima_posta != cantidad_postas - 1:
         postas_hitratacion.append(mapa_carrera[-1])
              
     return postas_hitratacion
