78674dd0229e12c8faa2f7219fa6be2942b9501b
Andres Moyano <125924128+MegaPZD@users.noreply.github.com>
2025-05-18 20:11:01 -0300

    Delete generador_sumas.py

--- generador_sumas.py
+++
@@ -1,7 +0,0 @@
-import random
-from suma_encadenada import suma_encadenada_minima
-
-n = random.randint(500, 1500)
-solucion = suma_encadenada_minima(n)
-
-print(f"Número n:{n}\nSolución:\n{solucion}\n")
