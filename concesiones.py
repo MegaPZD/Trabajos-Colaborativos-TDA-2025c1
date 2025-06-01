cf0bc4584d8b51e3612c9442b261806caca76c9c
Bauti Corti <63504751+Sando-dev@users.noreply.github.com>
2025-06-01 13:42:06 -0300

    Update concesiones.py

--- concesiones.py
+++ concesiones.py
@@ -3,8 +3,6 @@ import pulp as p
 # Creamos una instancia del problema de Programacion Lineal / Declaramos el Modelo:
 modelo = p.LpProblem("Concesiones Argentina 2000 SRL", p.LpMaximize)
 
-# Creamos de variables:
-
 #Variables binarias:
 c_a = p.LpVariable("Cliente A", cat = "Binary")
 c_b1 = p.LpVariable("Cliente B primera oferta", cat = "Binary")
@@ -15,17 +13,16 @@ c_e = p.LpVariable("Cliente E", cat = "Binary")
 c_f = p.LpVariable("Cliente F", cat = "Binary")
 c_g = p.LpVariable("Cliente G", cat = "Binary")
 
-# Definimos la funcion objetivo
+#funcion objetivo
 modelo += 50000 * c_a + 100000 * c_b1 + 120000 * c_b2 +  100000 * c_c + 80000 * c_d + 5000 * c_e + 40000 * c_f + 90000 * c_g, "Beneficio Total"
 
-# Definimos las restricciones
+#restricciones
 modelo += 30 * c_a + 80 * c_b1 + 120 * c_b2 + 75 * c_c + 50 * c_d + 2 * c_e + 20 * c_f + 100 * c_g <= 200, "Restriccion sobre la cantidad de paradas" 
 
 modelo += c_b1 + c_b2 <= 1, "Restriccion sobre el cielnte B, solo puede haber una oferta"
 
 modelo += c_a + c_d <= 1, "Restriccion por incompatibilidad entre los clientes A y D"
  
-# Resolvemos el problema
 modelo.solve()
 
 with open("resultado_concesiones.txt", "w") as f:
